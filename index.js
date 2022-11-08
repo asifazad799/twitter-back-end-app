8756620637b18e5b31c96846aa7e25a4463f8983
Mapout Admin <asif@mapout.com>
2022-11-09 04:08:44 +0530

    - added port details to console

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const routes = require("./src/routes/app");
-require('dotenv').config()
+require("dotenv").config();
 
 app.use("/", routes);
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080, () => {
+  console.log(
+    "\x1b[33m%s\x1b[0m",
+    `App running on port ${process.env.PORT || 8080}`
+  );
+});
